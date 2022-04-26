07a09a9694d910c161adb9a3c51fa813cd75144d
AlbertWebs <albertmuhatia@gmail.com>
2022-04-26 12:22:20 +0300

    Schema data

--- resources/views/front/master.blade.php
+++ resources/views/front/master.blade.php
@@ -318,6 +318,38 @@
     <script src="../../translate.google.com/translate_a/elementa0d8.js?cb=googleTranslateElementInit"></script>
     <script src="{{asset('venshaq/js/color-switcher.js')}}"></script>
 
+    {{-- Schema --}}
+    <script type='application/ld+json'>
+        {
+          "@context": "http://www.schema.org",
+          "@type": "ProfessionalService",
+          "name": "Venshaq Holdings Limited",
+          "url": "http://venshaq.com",
+          "logo": "https://venshaq.com/venshaq/images/Venshaq.png",
+          "sameAs": [
+            "https://www.facebook.com/venshaqholdings",
+            "https://www.instagram.com/venshaqholdings/",
+            "https://www.linkedin.com/company/venshaq-holdings-limited",
+            "https://twitter.com/venshaq",
+          ],
+          "priceRange": "$$$$",
+          "image": "https://venshaq.com/venshaq/images/Venshaq.png",
+          "description": "venshaq holding is a private holding company for shemaq production, shaqs house , treven cleaners limited and prosborders construction limited the short term and long term businesses of the group.
+          ",
+          "address": {
+             "@type": "PostalAddress",
+             "streetAddress": "Malik Heights",
+             "addressLocality": "Nairobi",
+             "addressRegion": "Kenya",
+             "postalCode": "00100",
+             "addressCountry": "Kenya"
+          },
+           "openingHours": "Mo 01:00-01:00 Tu 01:00-01:00 We 01:00-01:00 Th 01:00-01:00 Fr 01:00-01:00 Sa 01:00-01:00 Su 01:00-01:00",
+           "telephone": "+1202540 0134"
+        }
+    </script>
+    {{-- Schema --}}
+
 </body>
 
 </html>

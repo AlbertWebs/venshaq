f60a4f0e25cdb2ba20058c1bfd508023b7dea74f
AlbertWebs <albertmuhatia@gmail.com>
2022-05-10 07:26:39 +0300

    Update Home

---
+++ resources/views/front/Ilustrations.blade.php
@@ -0,0 +1,98 @@
+{{-- Ilustrations --}}
+<section class="gallery-section">
+    <div class="auto-container">
+        <!--MixitUp Galery-->
+        <div class="mixitup-gallery">
+            <!--Filter-->
+
+            <div class="filter-list row" id="MixItUp9297DA">
+                <!-- Gallery Item -->
+                <div class="gallery-item mix all web-design col-lg-4 col-md-6 col-sm-12" style="display: inline-block;">
+                    <div class="inner-box">
+                        <figure class="image"><img src="{{asset('venshaq/images/illustrations/1.jpg')}}" alt=""></figure>
+                        <a href="{{asset('venshaq/images/illustrations/1.jpg')}}" class="lightbox-image overlay-box" data-fancybox="gallery"></a>
+                        {{-- <div class="cap-box">
+                            <div class="cap-inner">
+                                <div class="cat"><span>Graphic</span></div>
+                                <div class="title">
+                                    <h5><a href="portfolio-single.html">Fimlor Experience</a></h5>
+                                </div>
+                            </div>
+                        </div> --}}
+                    </div>
+                </div>
+
+                <!-- Gallery Item -->
+                <div class="gallery-item mix all photography web-design col-lg-4 col-md-6 col-sm-12" style="display: inline-block;">
+                    <div class="inner-box">
+                        <figure class="image"><img src="{{asset('venshaq/images/illustrations/2.jpg')}}" alt=""></figure>
+                        <a href="{{asset('venshaq/images/illustrations/2.jpg')}}" class="lightbox-image overlay-box" data-fancybox="gallery"></a>
+                        {{-- <div class="cap-box">
+                            <div class="cap-inner">
+                                <div class="cat"><span>Graphic</span></div>
+                                <div class="title">
+                                    <h5><a href="portfolio-single.html">Fimlor Experience</a></h5>
+                                </div>
+                            </div>
+                        </div> --}}
+                    </div>
+                </div>
+
+                <!-- Gallery Item -->
+                <div class="gallery-item mix all branding web-design col-lg-4 col-md-6 col-sm-12" style="display: inline-block;">
+                    <div class="inner-box">
+                        <figure class="image"><img src="{{asset('venshaq/images/illustrations/3.jpg')}}" alt=""></figure>
+                        <a href="{{asset('venshaq/images/illustrations/3.jpg')}}" class="lightbox-image overlay-box" data-fancybox="gallery"></a>
+                        {{-- <div class="cap-box">
+                            <div class="cap-inner">
+                                <div class="cat"><span>Graphic</span></div>
+                                <div class="title">
+                                    <h5><a href="portfolio-single.html">Fimlor Experience</a></h5>
+                                </div>
+                            </div>
+                        </div> --}}
+                    </div>
+                </div>
+
+                <!-- Gallery Item -->
+                <div class="gallery-item mix all branding illustration col-lg-4 col-md-6 col-sm-12" style="display: inline-block;">
+                    <div class="inner-box">
+                        <figure class="image"><img src="{{asset('venshaq/images/illustrations/4.jpg')}}" alt=""></figure>
+                        <a href="{{asset('venshaq/images/illustrations/4.jpg')}}" class="lightbox-image overlay-box" data-fancybox="gallery"></a>
+                        {{-- <div class="cap-box">
+                            <div class="cap-inner">
+                                <div class="cat"><span>Graphic</span></div>
+                                <div class="title">
+                                    <h5><a href="portfolio-single.html">Fimlor Experience</a></h5>
+                                </div>
+                            </div>
+                        </div> --}}
+                    </div>
+                </div>
+
+                <!-- Gallery Item -->
+                <div class="gallery-item mix all branding illustration photography web-design col-lg-4 col-md-6 col-sm-12" style="display: inline-block;">
+                    <div class="inner-box">
+                        <figure class="image"><img src="{{asset('venshaq/images/illustrations/5.jpg')}}" alt=""></figure>
+                        <a href="{{asset('venshaq/images/illustrations/5.jpg')}}" class="lightbox-image overlay-box" data-fancybox="gallery"></a>
+                        {{-- <div class="cap-box">
+                            <div class="cap-inner">
+                                <div class="cat"><span>Graphic</span></div>
+                                <div class="title">
+                                    <h5><a href="portfolio-single.html">Fimlor Experience</a></h5>
+                                </div>
+                            </div>
+                        </div> --}}
+                    </div>
+                </div>
+
+                <!-- Gallery Item -->
+
+
+            </div>
+
+        </div>
+
+    </div>
+</section>
+{{-- Illustrations --}}
